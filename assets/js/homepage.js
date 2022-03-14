62ddbf1b56458b6647d3eebb79687e3f65b1d7b8
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-03-14 16:47:17 -0700

    handle form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,19 @@
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+
+let formSubmitHandler = (event) => {
+    event.preventDefault();
+    let username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+}
+
 let getUserRepos = (users) => {
     let apiUrl = "https://api.github.com/users/" + users + "/repos";
 
@@ -8,4 +24,4 @@ let getUserRepos = (users) => {
     })
 }
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
