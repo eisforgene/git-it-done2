4d21ba42383e18791c80e8652a7ccd98d9880754
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-03-15 15:18:23 -0700

    convert fetched data into dom elements

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,48 +1,50 @@
+// select the div container by using the id's
+
 let userFormEl = document.querySelector("#user-form");
 let nameInputEl = document.querySelector("#username");
 let repoContainerEl = document.querySelector("#repos-container");
 let repoSearchTerm = document.querySelector("#repo-search-term");
 
 
-let formSubmitHandler = (event) => {
-    event.preventDefault();
-    let username = nameInputEl.value.trim();
+let formSubmitHandler = (event) => { 
+    event.preventDefault(); // prevents submission of the form
+    let username = nameInputEl.value.trim(); // stores and trims the value of the nameInputEl which is selected by the username id within the form element  
 
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
+    if (username) { // if there is a username value
+        getUserRepos(username); // getUserRepos function is invoked with the username as the argument
+        nameInputEl.value = ""; // the input value is reset to an empty ""
+    } else { // if there is no username, return this alert
         alert("Please enter a GitHub username");
     }
-    console.log(event);
+    console.log(event); 
 }
 
-let getUserRepos = (users) => {
-    let apiUrl = "https://api.github.com/users/" + users + "/repos";
+let getUserRepos = (users) => { 
+    let apiUrl = "https://api.github.com/users/" + users + "/repos"; // dynamically sets repo address into apiUrl variable
 
-    fetch(apiUrl)
-    .then(response => {
-        if (response.ok) {
-            response.json().then(data => {
+    fetch(apiUrl) // uses the fetch promise with apiUrl as the parameter
+    .then(response => { // returns promise as a 'response' object
+        if (response.ok) { // if response is available (.ok) then the reponse object is returned in json format
+            response.json().then(data => { // then the promise will invoke a callback function called displayRepos handling two parameters (data from the response) and users
                 displayRepos(data, users);
             });  
         } else {
             alert("Error: GitHub User Not Found");
         }
     })
-    .catch(error => {
+    .catch(error => { // if there is an error, the alert will invoke
         alert("Unable to connect to GitHub");
     });
 }
 
-let displayRepos = (repos, searchTerm) => {
-    if (repos.length === 0) {
-        repoContainerEl.textContent = "No repositories found";
-        return;
+let displayRepos = (repos, searchTerm) => { // displayRepos function contains two parameters
+    if (repos.length === 0) { // if there are no repos
+        repoContainerEl.textContent = "No repositories found"; // return this text into repoContainerEl
+        return; // exit out of the function
     }
 
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
+    repoContainerEl.textContent = "";  // set repoContainerEl as an empty ""
+    repoSearchTerm.textContent = searchTerm; // set repoSearchTerm's textContent as the searchTerm variable
 
     for (let i = 0; i <repos.length; i++) {
         // format repo name
@@ -52,9 +54,9 @@ let displayRepos = (repos, searchTerm) => {
         let repoEl = document.createElement('div');
         repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-        /// create span element to hold repository name
+        // create span element to hold repository name
         let titleEl = document.createElement('span');
-        titleEl.textContent = repoName;
+        titleEl.textContent = repoName; // textContent of titleEl is same as the repoName
 
         repoEl.appendChild(titleEl);
 
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,46 +1,46 @@
-let issueContainerEl = document.querySelector('#issues-container');
+let issueContainerEl = document.querySelector('#issues-container'); // select the div container based on #issues-container id
 
-let getRepoIssues = (repo) => {
-    let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl)
-        .then(response => {
-            if (response.ok) {
-                response.json().then(data => {
+let getRepoIssues = (repo) => { // getRepoIssues function will pass a parameter, repo (name of the repository)
+    let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"; // dynamically create repo address and store in apiUrl variable
+    fetch(apiUrl) // fetch the apiUrl through its repo address and then chain a .then to fulfill the fetch promise
+        .then(response => { 
+            if (response.ok) { // if there is a response from the fetch (.ok) return the response promise in json format
+                response.json().then(data => { // fulfill promise by running the displayIssues function with the .then and (data) parameter.
                     displayIssues(data);
                 })
-            } else {
+            } else { // if response is unsuccessful return this alert
                 alert("There was a problem with your request!");
             }
         });
 }
 
 let displayIssues = (issues) => {
-    if (issues.length === 0) {
+    if (issues.length === 0) { // if issues.length equals 0, fill the textContent with it saying 'This repo has no open issues!' then return to stop the function from executing again
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
     for (let i = 0; i < issues.length; i++) {
-        let issueEl = document.createElement('a');
+        let issueEl = document.createElement('a'); // hyperlink
         issueEl.classList = 'list-item flex-row justify-space-between align-center'; // assigning classes
         issueEl.setAttribute('href', issues[i].html_url); // grabbing property (html_url) of issues object at [i] index
         issueEl.setAttribute('target', '_blank'); // creates a new tab instead of replacing current webpage
 
-        let titleEl = document.createElement('span');
-        titleEl.textContent = issues[i].title;
+        let titleEl = document.createElement('span'); //create span element
+        titleEl.textContent = issues[i].title; // set span element as the property (title) of issues object at [i] as the text content.
 
-        issueEl.appendChild(titleEl);
+        issueEl.appendChild(titleEl); // append new span element into issueEl (a attribute);
 
-        let typeEl = document.createElement('span');
+        let typeEl = document.createElement('span'); // create another span element
 
-        if (issues[i].pull_request) {
+        if (issues[i].pull_request) { // if the object issues at [i] index has a property named pull_request, the textContent for the new span element will = 'Pull Request' otherwise it'll be 'Issues'
             typeEl.textContent = "(Pull request)";
         } else {
             typeEl.textContent = "(Issue)";
         }
 
-        issueEl.appendChild(typeEl);
+        issueEl.appendChild(typeEl); // append the typeEl span element to issueEl = the a attribute
 
-        issueContainerEl.appendChild(issueEl);
+        issueContainerEl.appendChild(issueEl); // append issueEl span element into issueContainerEl, which selects the div container by the id #issues-container and appends it into it
     }
 }
 getRepoIssues("eisforgene/wordle");
